587e53a37ded3fd877133ed84477871ed7eabcc5
Duwe Aynab <mfaynab88@gmail.com>
2022-05-16 20:32:32 -0400

    ReduxCounter

--- src/index.js
+++ src/index.js
@@ -3,15 +3,15 @@ import ReactDOM from 'react-dom/client';
 //import './index.css';
 //import App from './App';
 import reportWebVitals from './reportWebVitals';
-import StateCounter from  "./components/StateCounter";
+import ReduxCounter from './components/ReduxCounter';
 
-
+import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-   <StateCounter />
-  </React.StrictMode>
+  <Provider>
+   <ReduxCounter />
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
